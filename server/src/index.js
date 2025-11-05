78ce51d1a96ba8a45c84daa0503b982bb9e84341
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2025-11-05 22:21:42 +0530

    Added connect Db

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,17 @@
 import dotenv from 'dotenv';
 import app from './app.js';
+import connectDb from './DB/index.js';
 dotenv.config();
 
 const port = process.env.PORT;
 
-app.listen(port, () => {
-  console.log(`Server Listening in on http://localhost:${port}`);
-});
+connectDb()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server Listening in on http://localhost:${port}`);
+    });
+  })
+  .catch((e) => {
+    console.error('Mongo COnnection Error');
+    process.exit(1);
+  });
