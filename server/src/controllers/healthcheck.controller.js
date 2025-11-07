0ec99763e0472e7de278c00bd9bd36ed5e7e64cd
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2025-11-08 00:02:26 +0530

    Used the async handler util instead of try-catch

--- server/src/controllers/healthcheck.controller.js
+++ server/src/controllers/healthcheck.controller.js
@@ -1,9 +1,17 @@
 import { ApiResponse } from '../utils/api-response.js';
+import { asyncHandler } from '../utils/async-handler.js';
 
-const healthCheck = (req, res) => {
-  try {
-    res.status(200).json(new ApiResponse(200, { message: 'Server Running Healthly...' }));
-  } catch (error) {}
-};
+// const healthCheck = (req, res) => {
+//   try {
+//     res
+//     .status(200)
+//     .json(new ApiResponse(200, { message: 'Server Running Healthly...' }));
+//   } catch (error) {}
+// };
 
-export {healthCheck};
+const healthCheck = asyncHandler(async (req, res) => {
+  res
+  .status(200)
+  .json(new ApiResponse(200, { message: 'Server Is Running Healthly...' }));
+});
+export { healthCheck };
